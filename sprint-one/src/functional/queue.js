e252a66bdf0233e5dddb3dfa1fdbcb648c936a78
Gabs00 <gewen87@gmail.com>
2014-07-23 18:53:13 -0400

    queue completed passing all

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,43 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    //add an element to storage, increment size
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function(){
+    //Check if queue is empty
+    if(size <= 0){
+      return 0;
+    }
+    //Get the first element
+    var index = 0;
+    var item = storage[index];
+
+    //delete it from the queue
+    delete storage[index];
+
+    //decrement
+    size--;
+    //loop through remaining keys, reassing index starting at 0
+    var tmp = {};
+    for(var i = 0; i < size; i++){
+        tmp[i] = storage[i+1];
+    }
+
+    storage = tmp;
+    return item;
+    
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
