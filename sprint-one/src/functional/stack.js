12dfb2fef400d4cd7a7f0d2449299a4667baff89
Gabs00 <gewen87@gmail.com>
2014-07-23 19:30:21 -0400

    completed stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,38 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
+
+  //Adds an item to the top of the stack
   someInstance.push = function(value){
+    //Add element to storage, using length as key
+    storage[size] = value;
+
+    //increment length
+    size++;
   };
 
+  //returns the item at the top of the stack
   someInstance.pop = function(){
+    //test length is not 0
+    if(size <= 0){
+      return undefined;
+    }
+
+    //decrement size
+    size--;
+
+    //Get the item at the top
+    var item = storage[size];
+    delete storage[size];
+
+    return item;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
