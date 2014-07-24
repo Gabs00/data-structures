f6558495c1b0193e53a64cd9b2ff33e1f16a6eec
Gabs00 <gewen87@gmail.com>
2014-07-23 21:34:34 -0400

    completed proto stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,41 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //define object
+  var obj = Object.create(stackMethods);
+  obj.length = 0;
+  return obj;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value){
+    //add to the top of stack using length
+    this[this.length] = value;
+
+    //increment length
+    this.length++;
+  };
+stackMethods.pop = function(){
+    //Check if empty
+    if(this.length <= 0){
+      return undefined;
+    }
+    //retrieve the topmost element
+    var item = this[this.length-1];
+
+    //decrement length
+    this.length--;
+
+    delete this[this.length];
+
+    return item;
+  };
+
+stackMethods.size= function(){
+    return this.length;
+  };
+
 
 
