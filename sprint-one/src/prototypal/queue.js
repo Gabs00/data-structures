48220355460b1bbc6edeb1fed61b2d67931d97fa
Gabs00 <gewen87@gmail.com>
2014-07-24 14:56:02 -0400

    completed proto queue, started psuedo stack

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,39 @@
 var makeQueue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  //create an instance 
+
+  var storage = Object.create(queueMethods);
+  storage.length = 0;
+  return storage;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value){
+  //Adds an item to the queue
+  this[this.length] = value;
+  this.length++;
+};
+
+queueMethods.dequeue = function(){
+  //Removes an item from the bottom of queue
+
+  //check if queue is empty
+  if(this.length <= 0){
+    return undefined;
+  }
+  var item = this[0];
+  this.length--;
+  for(var i = 0; i < this.length; i++){
+    this[i] = this[i+1];
+  }
+
+  delete this[this.length];
+  return item;
+};
+
+queueMethods.size = function(){
+  return this.length;
+};
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,47 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  //check if there
+  //if there are, var storage
+  
+  //declare a length property
+  this.length = arguments.length;
+
+  //if there are arguments, extend this with the argument properties
+  if(arguments.length > 0){
+      for( var i = 0; i < arguments.length; i++ ){
+          this[i] = arguments[i]
+      }
+  }
+  
+  
+};
+/*
+Stack.prototype = {};
+
+Stack.prototype.constructor = new Stack();*/
+
+Stack.prototype.push = function(value){
+    //add item to the top
+    
+    this[this.length] = value;
+    
+    this.length++;
 };
 
+Stack.prototype.pop = function(){
+    if(this.length <= 0) return undefined;
+    
+    this.length--;
 
+    var item = this[this.length];
+    
+    delete this[this.length];
+    
+    return item;
+};
+
+Stack.prototype.size = function(){
+    return this.length;
+};
