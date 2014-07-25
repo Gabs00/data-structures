c7b4a5d794d7df7821de422bd5bdafe2398eae64
Gabs00 <gewen87@gmail.com>
2014-07-25 02:24:21 -0400

    completed psuedo / stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,27 +1,10 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  
-  //check if there
-  //if there are, var storage
-  
-  //declare a length property
-  this.length = arguments.length;
-
-  //if there are arguments, extend this with the argument properties
-  if(arguments.length > 0){
-      for( var i = 0; i < arguments.length; i++ ){
-          this[i] = arguments[i]
-      }
-  }
-  
-  
+  this.length = 0;
 };
-/*
-Stack.prototype = {};
 
-Stack.prototype.constructor = new Stack();*/
-
+Stack.prototype.length;
 Stack.prototype.push = function(value){
     //add item to the top
     
